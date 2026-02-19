d4f386e58590b3598a224af8723490476a848624
Sajad Torkamani <sajadtorkamani1@gmail.com>
2026-02-19 19:18:11 +0000

    wip

--- app/root.tsx
+++ app/root.tsx
@@ -32,6 +32,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Meta />
         <Links />
       </head>
+
       <body>
         {children}
         <ScrollRestoration />
